d3167fb52cce7c67db4f1614c1be10ea9056af7c
Vincent Ockers <vincentbockers@gmail.com>
2025-10-19 21:43:11 +0200

    Declare pbkdf2 function

---
+++ src/pbkdf2.c
@@ -0,0 +1,13 @@
+#include "ft_ssl.h"
+
+// u8* pbkdf2(t_hmac_func hmac,
+//            const u8*   password,
+//            usize       password_len,
+//            const u8*   salt,
+//            usize       salt_len,
+//            u32         iterations,
+//            usize       dk_len)
+// {
+//     if (dk_len == 0 || iterations == 0 || hmac == NULL || password == NULL || salt == NULL)
+//         return NULL;
+// }
